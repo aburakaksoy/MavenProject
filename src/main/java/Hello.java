756cb78133091cd8b000b5f235e7f558e2c1287f
Abdullah Burak Aksoy <aburakaksoy@hotmail.com>
2021-12-17 20:08:46 +0300

    added mor hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing Hello Feature");
+        System.out.println("GitHub Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
+
 }
